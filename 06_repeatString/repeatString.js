883a443b35a078d79fba33257dac277a18f7152f
arthurguerraa <127884471+arthurguerraa@users.noreply.github.com>
2025-08-07 21:51:05 -0300

    exercicio 5 e 6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const palavra = 'hey';
+const vezes = 3;
+const repeatString = function(palavra, vezes) {
+    if(vezes < 0) return "ERROR";
+    let resultado = '';
+    for(let i = 0; i < vezes; i++){
+        resultado += palavra;
+    }
+    return resultado;
 };
 
 // Do not edit below this line
