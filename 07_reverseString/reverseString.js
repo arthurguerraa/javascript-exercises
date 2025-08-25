323492e8cf3877da5c4183642687114a70e22a47
arthurguerraa <127884471+arthurguerraa@users.noreply.github.com>
2025-08-25 18:48:46 -0300

    exercicio reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,18 @@
-const reverseString = function() {
+const reverseString = function (palavra) {
+    let vet = palavra.split("");
+    let inicio = 0;
+    let fim = vet.length - 1;
 
+    while(inicio < fim){
+        let temp = vet[inicio];
+        vet[inicio] = vet[fim];
+        vet[fim] = temp;
+        inicio++;
+        fim--;
+    }
+
+    let palavraInvertida = vet.join("");
+    return palavraInvertida;
 };
 
 // Do not edit below this line
