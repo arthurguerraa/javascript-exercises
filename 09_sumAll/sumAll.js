bb0033f57acaa3c703dfe026f243b7b078e9d75b
arthurguerraa <127884471+arthurguerraa@users.noreply.github.com>
2025-08-25 20:52:32 -0300

    exercicio sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let soma = 0;
+    
+    if(a < 0 || b < 0 || Number.isInteger(a) == false || Number.isInteger(b) == false){
+        return "ERROR";
+    }else{
+        if(a > b){
+        for(let i = b; i <= a; i++){
+        soma += i;
+    }}else{
+        for(let i = a; i <= b; i++){
+        soma += i;
+    }}
+    }
+    
+    return soma;
 };
 
 // Do not edit below this line
