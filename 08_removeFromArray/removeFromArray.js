3ca457d202d4ec5d085e599bb8138be7888a2a6f
arthurguerraa <127884471+arthurguerraa@users.noreply.github.com>
2025-08-25 19:27:56 -0300

    exercicio removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(vetor, valorARemover, valorARemover2, a, b) {
+
+    const vetorFiltrado = vetor.filter(temp => temp !== valorARemover && temp !== valorARemover2);
+    const vetorSemMultiplos = vetorFiltrado.filter(temp => temp !== valorARemover % 2 == 0);
+    const vetorSemNada = vetor.filter(temp => temp !== valorARemover && temp !== valorARemover2 && temp !== a && temp !== b);
+    return vetorSemNada;
+    return vetorFiltrado;
+    return vetorSemMultiplos;
 };
 
 // Do not edit below this line
