b26824359a57b2749633d41f88e8a591b0e5e1db
arthurguerraa <127884471+arthurguerraa@users.noreply.github.com>
2025-08-25 21:13:56 -0300

    exercicio leapYears

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(ano) {
+    if(ano % 400 == 0){
+        return true;
+    }else if(ano % 100 == 0){
+        return false;
+    }else if(ano % 4 == 0){
+        return true;
+    }else{
+        return false;
+    }
 };
 
 // Do not edit below this line
