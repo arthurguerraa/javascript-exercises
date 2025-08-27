78857ef2bc98ff3122ef5e4ceb081f79f4f9d04f
arthurguerraa <127884471+arthurguerraa@users.noreply.github.com>
2025-08-27 15:55:46 -0300

    exercicio palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (palavra) {
+    let palavraInvertida = palavra.toLowerCase().match(/[a-z0-9]/gi).reverse().join(); 
 
+    let palavraNormalizada = palavra.toLowerCase().match(/[a-z0-9]/gi).join();
+    
+    if(palavraNormalizada === palavraInvertida){
+        return true;
+    }else{
+        return false;
+    }
+    
 };
 
 // Do not edit below this line
