51a92a0a4f3e264c180ce9f826ac8bcf6c92f996
arthurguerraa <127884471+arthurguerraa@users.noreply.github.com>
2025-08-27 17:43:54 -0300

    exercicio fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    if(a < 0){
+        return "OOPS";
+    }else if(a == 0){
+        return 0;
+    }else if(a == 1 || a == 2){
+        return 1;
+    }else{
+        let primeiro = 1;
+        let segundo = 1;
+        let soma;
+        for(let i = 3; i <= a; i++){
+            soma = primeiro + segundo
+            primeiro = segundo;
+            segundo = soma;
+        }
+        return soma;
+    }
+    
 };
 
 // Do not edit below this line
