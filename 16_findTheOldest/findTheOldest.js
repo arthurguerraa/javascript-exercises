fc939e07636b915ca170eaa2cbc7ccea059e0cdd
arthurguerraa <127884471+arthurguerraa@users.noreply.github.com>
2025-08-28 16:14:07 -0300

    exercicio findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+   const anoAtual = new Date().getFullYear();
 
+  return array.reduce((maisVelha, pessoa) => {
+    const idadeMaisVelha = (maisVelha.yearOfDeath || anoAtual) - maisVelha.yearOfBirth;
+    const idadeAtual = (pessoa.yearOfDeath || anoAtual) - pessoa.yearOfBirth;
+
+    return idadeAtual > idadeMaisVelha ? pessoa : maisVelha;
+  });
 };
 
 // Do not edit below this line
